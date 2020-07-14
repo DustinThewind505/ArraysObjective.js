134b1217b8dab00b4d30f35fb405b3ef6c4037cf
unknown <Dustin.Guillen14@yahoo.com>
2020-07-13 22:09:21 -0600

    list comprehension

--- practice.py
+++ practice.py
@@ -1,10 +1,24 @@
-x = 321
-y = 154323
+# x = 321
+# y = 154323
 
-if x == y:
-    print(f'{x} and {y} are equal')
-else:
-    if x < y:
-        print(f'{x} is less than {y}')
-    else:
-        print(f'{x} is greater than {y}')
+# if x == y:
+#     print(f'{x} and {y} are equal')
+# else:
+#     if x < y:
+#         print(f'{x} is less than {y}')
+#     else:
+#         print(f'{x} is greater than {y}')
+
+### ===================================== Lists(arrays) =====================================
+evens = []
+
+for i in range(501):
+    if i % 10 == 0:
+        evens.append(i)
+
+print(evens)
+
+### ====== List Comprhension(short hand) ======
+odds = [i for i in range(501) if i % 30 == 0]
+
+print(odds)
