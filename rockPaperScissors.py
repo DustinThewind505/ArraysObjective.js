eda61b979bfdaa579be894bba15b3b6246c8d67e
unknown <Dustin.Guillen14@yahoo.com>
2020-07-14 22:45:23 -0600

    set up rock paper scissors

---
+++ rockPaperScissors.py
@@ -0,0 +1,26 @@
+import random
+
+score = {
+    "wins": 0,
+    "losses": 0,
+    "ties": 0
+}
+
+welcome_message = “Welcome to Rock, Paper, Scissors!”
+historical_data_message = “Wins: %s, Ties: %s, Losses: %s”
+quit_message = “Thanks for playing Rock, Paper, Scissors!”
+win_message = “Congratulations, you won!”
+loss_message = “Sorry, you lost!”
+tie_message = “It was a tie.”
+
+historical_data =
+score[“wins”] = historical_data[“wins”]
+score[“ties”] = historical_data[“ties”]
+score[“losses”] = historical_data[“losses”]
+
+choice_options = {
+    1: “rock”,
+    2: “paper”,
+    3: “scissors”,
+    9: “quit”
+}
