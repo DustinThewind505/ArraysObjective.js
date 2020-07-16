d6a1b5efb11e774ebedf65fe250afdc0bdb9b5d4
unknown <Dustin.Guillen14@yahoo.com>
2020-07-15 21:50:14 -0600

    import random/ used random.choice

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,29 +1,9 @@
 import random
 
-score = {
-    "wins": 0,
-    "losses": 0,
-    "ties": 0
-}
+possible_choices = ["rock", "paper", "scissors"]
+programs_choice = random.choice(possible_choices)
+print(f'\n\n\t\t\t\t***{programs_choice}')
 
-welcome_message = “Welcome to Rock, Paper, Scissors!”
-historical_data_message = “Wins: %s, Ties: %s, Losses: %s”
-quit_message = “Thanks for playing Rock, Paper, Scissors!”
-win_message = “Congratulations, you won!”
-loss_message = “Sorry, you lost!”
-tie_message = “It was a tie.”
+users_choice = input(f"\t\t\t\t***Choose rock, paper or scissors: ")
+print(f'\t\t\t\t***{users_choice}\n')
 
-historical_data =
-score[“wins”] = historical_data[“wins”]
-score[“ties”] = historical_data[“ties”]
-score[“losses”] = historical_data[“losses”]
-
-choice_options = {
-    1: “rock”,
-    2: “paper”,
-    3: “scissors”,
-    9: “quit”
-}
-
-computer_choice = random.randint(1, 3) # use the random module imported above
-user_choice = None
