c142178ba002d8f36b78f862c1d7f45158028a38
unknown <Dustin.Guillen14@yahoo.com>
2020-07-16 10:22:07 -0600

    added score

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,8 +1,16 @@
 import random
 
+# =============== Message variables ===============
 # winner_alert = print(f'\t\t\t\t\t\tYou win!\n')
 # loser_alert = print(f'\t\t\t\t\t\tYou lose\n')
 # tie_alert = print(f'\t\t\t\t\t\tTie\n')
+
+# =============== Message variables ===============
+score = {
+    "wins": 0,
+    "loses": 0,
+    "ties": 0
+}
 
 # =============== Computer Opponent ===============
 possible_choices = ["rock", "paper", "scissors"]
@@ -12,30 +20,40 @@ print(f'\n\n\t\t\t\t***{programs_choice}')
 
 # =============== User Choice ===============
 users_choice = input(f"\t\t\t\t***Choose rock, paper or scissors: ")
-print(f'\t\t\t\t***{users_choice}')
-
+print(f'\t\t\t\t***{users_choice}\n')
 
 # =============== Rock, Paper, Scissors logic ===============
 if users_choice == "rock":
     if programs_choice == "rock":
+        score["ties"] += 1
         print(f'\t\t\t\t\t\tTie\n')
     elif programs_choice == "paper":
+        score["loses"] += 1
         print(f'\t\t\t\t\t\tYou lose\n')
     elif programs_choice == "scissors":
+        score["wins"] += 1
         print(f'\t\t\t\t\t\tYou win!\n')
 elif users_choice == "paper":
     if programs_choice == "rock":
+        score["wins"] += 1
         print(f'\t\t\t\t\t\tYou win!\n')
     elif programs_choice == "paper":
+        score["ties"] += 1
         print(f'\t\t\t\t\t\tTie\n')
     elif programs_choice == "scissors":
+        score["loses"] += 1
         print(f'\t\t\t\t\t\tYou lose\n')
 elif users_choice == "scissors":
     if programs_choice == "rock":
+        score["loses"] += 1
         print(f'\t\t\t\t\t\tYou lose\n')
     elif programs_choice == "paper":
+        score["wins"] += 1
         print(f'\t\t\t\t\t\tYou win!\n')
     elif programs_choice == "scissors":
+        score["ties"] += 1
         print(f'\t\t\t\t\t\tTie\n') 
 else:
-    print(f'\t\t\t\t\t\tSure Buddy\n') 
+    print(f'\t\t\t\t\t\tSure Buddy')
+
+print(f'\t\t\t\tYour SCORE = {score}\n\n\n\n')
