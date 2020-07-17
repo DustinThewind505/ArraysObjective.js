ee475b9f7b688613a0e4b9ca7980ddbd774a2018
unknown <Dustin.Guillen14@yahoo.com>
2020-07-17 11:10:25 -0600

    encapsulation-abstraction-inheritance-polymorphism

--- classes.py
+++ classes.py
@@ -9,8 +9,22 @@ class Waypoint(LatLon):
         super().__init__(lat, lon)
         self.name = name
 
+    def __str__(self):
+        return f"Waypoint: {self.name}, {self.lat}, {self.lon}"
+
 class Geocache(Waypoint):
-    def __init__(self, name, difficulty, size, lat, lon)
-        super().__init__(name, lat lon)
+    def __init__(self, name, difficulty, size, lat, lon):
+        super().__init__(name, lat, lon)
         self.difficulty = difficulty
         self.size = size
+
+    def __str__(self):
+        return f"Geocache: {self.name}, {self.difficulty}, {self.size}, {self.lat}, {self.lon}"
+
+waypoint = Waypoint("Catacombs", 41.7050, -121.51521)
+
+print(waypoint)
+
+geocache =  Geocache("Newberry Views", "diff: 1.5", "size: 2", 44.05137, -121.41556)
+
+print(geocache)
