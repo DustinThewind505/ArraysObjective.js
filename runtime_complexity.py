b7a716324e673f15f912f4e04085de10651331e8
unknown <Dustin.Guillen14@yahoo.com>
2020-07-22 22:05:58 -0600

    Constant Time / Linear Time

--- runtime_complexity.py
+++ runtime_complexity.py
@@ -1,10 +1,17 @@
-### ======================= CONSTANT Time =======================
+### ====== LIST ======
 items = ["a", "b", "c", "d", "e"]
 
+### ======================= CONSTANT Time =======================
 def print_one(list):
     print(f'\n{items[0]}\n')
 
 print_one(items)
 
 ### ======================= LINEAR Time =======================
+def print_list(list):
+    for i in items:
+        print(i)
+
+print_list(items)
+
 ### ======================= QUADRATIC Time =======================
