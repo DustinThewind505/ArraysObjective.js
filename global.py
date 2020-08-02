e77521037f875f0fec95f5e7c5ceaecc4d9d171c
unknown <Dustin.Guillen14@yahoo.com>
2020-08-01 19:45:00 -0600

    basic variables

---
+++ global.py
@@ -0,0 +1,31 @@
+## ====== Variables ======
+array = ["Bob", "Linda", "Gene", "Mr. Fishowner"]
+
+name = "bob"
+age = 42
+
+
+
+## ====== Functions ======
+def use_Bob(num):
+    if num >= 55:
+        print(f'{name} is {str(age)}')
+    else:
+        print('Need a bigger number')
+
+
+def insert_Bob(str):
+    len(array)
+    array.append(str)
+    array.insert(str)
+    array.remove(array[0])
+    len(array)
+
+
+## ====== Printing ======
+use_Bob(55)
+
+insert_Bob("Tammy")
+
+# for name in array:
+#     print(array[3])
--- single-linked-list.py
+++ single-linked-list.py
@@ -1,22 +1,72 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value =None, next_node=None):
         self.value = value
-        self.next = None
+        self.next_node = next_node
 
     def get_value(self):
         return self.value
 
     def get_next(self):
-        return self.next
+        return self.next_node
 
     def set_next(self, new_next):
-        self.next = new_next
+        self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value, None)
+
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+    
+    def remove_head(self, index)
+        if not self.head:
+            return None
+        if not self.head.get_next:
+            head = self.headself
+            self.head = None
+            self.tail = None
+            return head.get_value
+        value = self.head.get_value
+        self.head = self.head.get_next
+
+    def get_max(self):
+        if not self.head:
+            return None
+
+        current = self.head
+        max_val = self.head.value
+        while current:
+            if current.value > max_val:
+                max_val = current.value
+            current = current.next_node
+        return max_val
 
 
-ll = Node(1)
-ll.next = Node(2)
-ll.next.next = Node(3)
-ll.next.next.next = Node(4)
-ll.next.next.next.next = Node(5)
+      
 
-print(ll.value)
+
+
+
+
+
+
+
+
+
+
+# ll = Node(1)
+# ll.next = Node(2)
+# ll.next.next = Node(3)
+# ll.next.next.next = Node(4)
+# ll.next.next.next.next = Node(5)
+
+# print(ll.value)
