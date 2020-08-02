468cf9b1b8bcc0d4261f46df1fc01b178d548337
unknown <Dustin.Guillen14@yahoo.com>
2020-08-01 22:22:47 -0600

    comments to hello world

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
+# ======== Print "Hello World" ========
+print('Hello World')
+
+
+# ======== Declare a variable ========
 name = "Monty Python"
 
-print(name)
+
+# ======== String concatenation ========
+print(f'Hello {name}')
+# or
+print('Hello ' + name)
 
 if name != "Sean":
     print("Hey!, You're not Sean!")
