1b81112aa1993d0501c497064ee848ad869f3753
unknown <Dustin.Guillen14@yahoo.com>
2020-08-02 13:45:26 -0600

    CRUD for objects

---
+++ dictionaries.py
@@ -0,0 +1,51 @@
+### ================== Python DICTIONARIES Basics ==================
+# ====== Variables ======
+object = {
+    "Co-Owner1": "Bob"
+    }
+
+name = "Linda"
+age = 33
+
+# ============ Sample Function ============
+def add_Linda(obj):
+    #print(f'\n{obj}')
+    obj["Co-Owner2"] = name
+    #print(f'{obj}\n')
+
+add_Linda(object)
+
+### ================== CRUD Functions ==================
+# ============ GET ============
+def get_Family(obj):
+    print(f'\n{obj}\n')
+    
+
+# ============ GET by key ============
+def get_Bob_by_key(obj, str):
+    print(obj[str])
+    
+
+# ============ POST ============
+def add_fam(obj, role, name):
+    obj[role] = name
+    print(f'\n{obj}\n')
+
+
+# ============ PUT ============
+
+# ============ DELETE ============
+def delete_Bob(obj):
+    del obj["Co-Owner1"]
+    print(obj)
+       
+
+
+### ================== Printing ==================
+get_Family(object)
+
+get_Bob_by_key(object, "Co-Owner1")
+
+add_fam(object, "Waitress", "Tina")
+
+delete_Bob(object)
