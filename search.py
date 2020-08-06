c542669198fb3b75bcdfd26461ddad2978721e0d
unknown <Dustin.Guillen14@yahoo.com>
2020-08-05 21:03:30 -0600

    Binary search

--- search.py
+++ search.py
@@ -1,23 +1,44 @@
+### ==================== IMPORTS ====================
 import random
 import time
+
+### ==================== VARIABLES ====================
 my_range = 100
 my_size = 15
-nums = list(range(0, my_size))
-shuffled_nums = list(range(0, my_size))
-random.shuffle(shuffled_nums)
 
-print(nums)
-print(shuffled_nums)
+random_nums = random.sample(range(my_range), my_size)
+
+print(random_nums)
+
 
 num_to_find = 14
 
-# ========== O(n) LINEAR TIME ==========
+# ========== LINEAR SEARCH *O(n) linear time* ==========
 def linear_search(arr, target):
     for num in arr:
         if num == target:
             return True
     return False
 
+random_nums.sort()
+print(random_nums)
 
-print(linear_search(nums, num_to_find))
-print(linear_search(shuffled_nums, 33))
+# ========== BINARY SEARCH ==========
+def binary_search(arr, target):
+    start = 0
+    end = (len(arr) - 1)
+
+    found = False
+    while end >= start and not found:
+        middle_index = (start + end) // 2
+        if arr[middle_index] == target:
+            found = True
+        else:
+            if target < arr[middle_index]:
+                end = middle_index - 1
+            if target > arr[middle_index]:
+                start = middle_index + 1
+
+    return found
+
+print(binary_search(random_nums, 13))
