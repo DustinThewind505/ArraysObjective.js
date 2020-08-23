0196dd34d1b6fbb9416d37ecf78b924755055136
unknown <Dustin.Guillen14@yahoo.com>
2020-08-23 12:06:28 -0600

    binary search tree

--- queue.py
+++ queue.py
@@ -1,8 +1,24 @@
 ### ==================== IMPORTS ====================
-
+from singly_Linkedlist import Linkedlist
 
 ### ==================== A QUEUE using a Linked List ====================
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = Linkedlist()
 
+    def __len__(self):
+        return self.size
+
+    def enqsueue(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()
 
 
 
